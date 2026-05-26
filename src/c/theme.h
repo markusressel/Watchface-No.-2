9080dae84638ae71bb00146ba58a9148df988f6e
Markus Ressel <mail@markusressel.de>
2026-05-26 19:18:57 +0200

    fix typo

--- src/c/theme.h
+++ src/c/theme.h
@@ -42,7 +42,7 @@ Theme* theme_get_theme();
 // @param showSeconds  specifies if the time shows seconds (to properly select font size)
 void init_theme(enum ThemeEnum theme, bool showSeconds);
 
-// method to initlalize custom theme with custom colors
+// method to initialize custom theme with custom colors
 // @param theme        the custom theme
 // @param showSeconds  specifies if the time shows seconds (to properly select font size)
 void init_custom_theme(Theme theme, bool showSeconds);
