544f0a496880b83ef02a284614ba1bba0671c78e
Markus Ressel <mail@markusressel.de>
2026-05-28 01:37:38 +0200

    refactor: remove redundant layout_changed flag and streamline row widget updates in inbox callback

--- src/c/app_messaging.c
+++ src/c/app_messaging.c
@@ -7,215 +7,204 @@
 static ClaySettings *s_settings;
 
 static void inbox_received_callback(DictionaryIterator *iterator, void *context) {
-  // Read clay configuration properties
+    // Read clay configuration properties
 
-  s_settings = clay_get_settings();
+    s_settings = clay_get_settings();
 
-  // Read theme
-  Tuple *theme_t = dict_find(iterator, MESSAGE_KEY_Theme);
-  if (theme_t) {
-    char *theme = theme_t->value->cstring;
-    strcpy(s_settings->ThemeValue, theme);
-  }
-
-  // Read color preferences
-  Tuple *bg_color_t = dict_find(iterator, MESSAGE_KEY_BackgroundColor);
-  if (bg_color_t) {
-    GColor bg_color = GColorFromHEX(bg_color_t->value->int32);
-    // APP_LOG(APP_LOG_LEVEL_DEBUG, "bg_color: old: %lu new: %lu", settings.BackgroundColor, bg_color);
-    s_settings->BackgroundColor = bg_color;
-  }
-
-  // Time Layer Colors
-  Tuple *text_color_time_t = dict_find(iterator, MESSAGE_KEY_TimeTextColor);
-  if (text_color_time_t) {
-    GColor text_color_time = GColorFromHEX(text_color_time_t->value->int32);
-    s_settings->TimeTextColor = text_color_time;
-  }
-
-  // Date Layer Colors
-  Tuple *text_color_date_t = dict_find(iterator, MESSAGE_KEY_DateTextColor);
-  if (text_color_date_t) {
-    GColor text_color_date = GColorFromHEX(text_color_date_t->value->int32);
-    s_settings->DateTextColor = text_color_date;
-  }
-
-  // Battery Bar Layer Colors
-  Tuple *battery_frame_color_t = dict_find(iterator, MESSAGE_KEY_BatteryFrameColor);
-  if (battery_frame_color_t) {
-    GColor battery_frame_color = GColorFromHEX(battery_frame_color_t->value->int32);
-    s_settings->BatteryFrameColor = battery_frame_color;
-  }
-
-  Tuple *battery_fill_color_t = dict_find(iterator, MESSAGE_KEY_BatteryFillColor);
-  if (battery_fill_color_t) {
-    GColor battery_fill_color = GColorFromHEX(battery_fill_color_t->value->int32);
-    s_settings->BatteryFillColor = battery_fill_color;
-  }
-
-  // Weather Layer Colors
-  Tuple *weather_text_color_t = dict_find(iterator, MESSAGE_KEY_WeatherTextColor);
-  if (weather_text_color_t) {
-    GColor weather_text_color = GColorFromHEX(weather_text_color_t->value->int32);
-    s_settings->WeatherTextColor = weather_text_color;
-  }
-
-  // Stepcount Layer Colors
-  Tuple *stepcount_text_color_t = dict_find(iterator, MESSAGE_KEY_StepcountTextColor);
-  if (stepcount_text_color_t) {
-    GColor stepcount_text_color = GColorFromHEX(stepcount_text_color_t->value->int32);
-    s_settings->StepcountTextColor = stepcount_text_color;
-  }
-
-  // Read Slider preferences
-  Tuple *dot_count_t = dict_find(iterator, MESSAGE_KEY_SliderDigitWidth);
-  if (dot_count_t) {
-    int dot_count = dot_count_t->value->int32;
-    s_settings->DigitWidth = dot_count;
-  }
-
-  Tuple *dot_width_t = dict_find(iterator, MESSAGE_KEY_SliderDotWidth);
-  if (dot_width_t) {
-    int dot_width = dot_width_t->value->int32;
-    s_settings->DotWidth = dot_width;
-  }
-
-  Tuple *dot_height_t = dict_find(iterator, MESSAGE_KEY_SliderDotHeight);
-  if (dot_height_t) {
-    int dot_height = dot_height_t->value->int32;
-    s_settings->DotHeight = dot_height;
-  }
-
-  Tuple *dot_horizontal_gap_t = dict_find(iterator, MESSAGE_KEY_SliderDotHorizontalGap);
-  if (dot_horizontal_gap_t) {
-    int dot_horizontal_gap = dot_horizontal_gap_t->value->int32;
-    s_settings->DotHorizontalGap = dot_horizontal_gap;
-  }
-
-  Tuple *dot_vertical_gap_t = dict_find(iterator, MESSAGE_KEY_SliderDotVerticalGap);
-  if (dot_vertical_gap_t) {
-    int dot_vertical_gap = dot_vertical_gap_t->value->int32;
-    s_settings->DotVerticalGap = dot_vertical_gap;
-  }
-
-
-  // Read boolean preferences
-  Tuple *show_year_t = dict_find(iterator, MESSAGE_KEY_ShowYear);
-  if (show_year_t) {
-    bool show_year = show_year_t->value->int32 == 1;
-    s_settings->ShowYear = show_year;
-  }
-
-  Tuple *show_seconds_t = dict_find(iterator, MESSAGE_KEY_ShowSeconds);
-  if (show_seconds_t) {
-    bool show_seconds = show_seconds_t->value->int32 == 1;
-    s_settings->ShowSeconds = show_seconds;
-  }
-
-  Tuple *show_weekday_t = dict_find(iterator, MESSAGE_KEY_ShowWeekdayAbbreviation);
-  if (show_weekday_t) {
-    bool show_weekday = show_weekday_t->value->int32 == 1;
-    s_settings->ShowWeekdayAbbreviation = show_weekday;
-  }
-
-  Tuple *weekday_uppercase_t = dict_find(iterator, MESSAGE_KEY_WeekdayAbbreviationUppercase);
-  if (weekday_uppercase_t) {
-    bool weekday_uppercase = weekday_uppercase_t->value->int32 == 1;
-    s_settings->WeekdayAbbreviationUppercase = weekday_uppercase;
-  }
-
-  // Read row layout preferences
-  bool layout_changed = false;
-
-  Tuple *row0_t = dict_find(iterator, MESSAGE_KEY_Row0Widget);
-  if (row0_t) {
-    // Read as a string and convert to integer
-    int value = atoi(row0_t->value->cstring);
-    if (s_settings->Row0Widget != value) {
-      s_settings->Row0Widget = value;
-      layout_changed = true;
+    // Read theme
+    Tuple *theme_t = dict_find(iterator, MESSAGE_KEY_Theme);
+    if (theme_t) {
+        char *theme = theme_t->value->cstring;
+        strcpy(s_settings->ThemeValue, theme);
     }
-  }
 
-  Tuple *row1_t = dict_find(iterator, MESSAGE_KEY_Row1Widget);
-  if (row1_t) {
-    int value = atoi(row1_t->value->cstring);
-    if (s_settings->Row1Widget != value) {
-      s_settings->Row1Widget = value;
-      layout_changed = true;
+    // Read color preferences
+    Tuple *bg_color_t = dict_find(iterator, MESSAGE_KEY_BackgroundColor);
+    if (bg_color_t) {
+        GColor bg_color = GColorFromHEX(bg_color_t->value->int32);
+        // APP_LOG(APP_LOG_LEVEL_DEBUG, "bg_color: old: %lu new: %lu", settings.BackgroundColor, bg_color);
+        s_settings->BackgroundColor = bg_color;
     }
-  }
 
-  Tuple *row3_t = dict_find(iterator, MESSAGE_KEY_Row3Widget);
-  if (row3_t) {
-    int value = atoi(row3_t->value->cstring);
-    if (s_settings->Row3Widget != value) {
-      s_settings->Row3Widget = value;
-      layout_changed = true;
+    // Time Layer Colors
+    Tuple *text_color_time_t = dict_find(iterator, MESSAGE_KEY_TimeTextColor);
+    if (text_color_time_t) {
+        GColor text_color_time = GColorFromHEX(text_color_time_t->value->int32);
+        s_settings->TimeTextColor = text_color_time;
     }
-  }
 
-  Tuple *row4_t = dict_find(iterator, MESSAGE_KEY_Row4Widget);
-  if (row4_t) {
-    int value = atoi(row4_t->value->cstring);
-    if (s_settings->Row4Widget != value) {
-      s_settings->Row4Widget = value;
-      layout_changed = true;
+    // Date Layer Colors
+    Tuple *text_color_date_t = dict_find(iterator, MESSAGE_KEY_DateTextColor);
+    if (text_color_date_t) {
+        GColor text_color_date = GColorFromHEX(text_color_date_t->value->int32);
+        s_settings->DateTextColor = text_color_date;
     }
-  }
 
-  // Read weather data
+    // Battery Bar Layer Colors
+    Tuple *battery_frame_color_t = dict_find(iterator, MESSAGE_KEY_BatteryFrameColor);
+    if (battery_frame_color_t) {
+        GColor battery_frame_color = GColorFromHEX(battery_frame_color_t->value->int32);
+        s_settings->BatteryFrameColor = battery_frame_color;
+    }
 
-  // Read tuples for data
-  Tuple *temp_cur_tuple = dict_find(iterator, MESSAGE_KEY_WEATHER_TEMPERATURE_CURRENT);
-  Tuple *temp_min_tuple = dict_find(iterator, MESSAGE_KEY_WEATHER_TEMPERATURE_MIN);
-  Tuple *temp_max_tuple = dict_find(iterator, MESSAGE_KEY_WEATHER_TEMPERATURE_MAX);
-  Tuple *condition_tuple = dict_find(iterator, MESSAGE_KEY_WEATHER_CONDITION);
+    Tuple *battery_fill_color_t = dict_find(iterator, MESSAGE_KEY_BatteryFillColor);
+    if (battery_fill_color_t) {
+        GColor battery_fill_color = GColorFromHEX(battery_fill_color_t->value->int32);
+        s_settings->BatteryFillColor = battery_fill_color;
+    }
 
-  // If all data is available, use it
-  if (temp_cur_tuple && temp_min_tuple && temp_max_tuple && condition_tuple) {
-    WeatherData *weatherData = weather_get_data();
+    // Weather Layer Colors
+    Tuple *weather_text_color_t = dict_find(iterator, MESSAGE_KEY_WeatherTextColor);
+    if (weather_text_color_t) {
+        GColor weather_text_color = GColorFromHEX(weather_text_color_t->value->int32);
+        s_settings->WeatherTextColor = weather_text_color;
+    }
 
-    APP_LOG(APP_LOG_LEVEL_DEBUG, "temp changed: old: %d new: %d", (int) weatherData->CurrentTemperature, (int)temp_cur_tuple->value->int32);
+    // Stepcount Layer Colors
+    Tuple *stepcount_text_color_t = dict_find(iterator, MESSAGE_KEY_StepcountTextColor);
+    if (stepcount_text_color_t) {
+        GColor stepcount_text_color = GColorFromHEX(stepcount_text_color_t->value->int32);
+        s_settings->StepcountTextColor = stepcount_text_color;
+    }
 
-    weatherData->CurrentTemperature = temp_cur_tuple->value->int32;
-    weatherData->MinTemperature = temp_min_tuple->value->int32;
-    weatherData->MaxTemperature = temp_max_tuple->value->int32;
-    strcpy(weatherData->CurrentConditions, condition_tuple->value->cstring);
+    // Read Slider preferences
+    Tuple *dot_count_t = dict_find(iterator, MESSAGE_KEY_SliderDigitWidth);
+    if (dot_count_t) {
+        int dot_count = dot_count_t->value->int32;
+        s_settings->DigitWidth = dot_count;
+    }
 
-    update_weather();
-  }
+    Tuple *dot_width_t = dict_find(iterator, MESSAGE_KEY_SliderDotWidth);
+    if (dot_width_t) {
+        int dot_width = dot_width_t->value->int32;
+        s_settings->DotWidth = dot_width;
+    }
 
-  // persist data
-  clay_save_settings();
+    Tuple *dot_height_t = dict_find(iterator, MESSAGE_KEY_SliderDotHeight);
+    if (dot_height_t) {
+        int dot_height = dot_height_t->value->int32;
+        s_settings->DotHeight = dot_height;
+    }
 
-  // Rebuild the layer tree if the row layout changed
-  if (layout_changed) {
-    APP_LOG(APP_LOG_LEVEL_INFO, "Row layout changed, reloading layers");
+    Tuple *dot_horizontal_gap_t = dict_find(iterator, MESSAGE_KEY_SliderDotHorizontalGap);
+    if (dot_horizontal_gap_t) {
+        int dot_horizontal_gap = dot_horizontal_gap_t->value->int32;
+        s_settings->DotHorizontalGap = dot_horizontal_gap;
+    }
+
+    Tuple *dot_vertical_gap_t = dict_find(iterator, MESSAGE_KEY_SliderDotVerticalGap);
+    if (dot_vertical_gap_t) {
+        int dot_vertical_gap = dot_vertical_gap_t->value->int32;
+        s_settings->DotVerticalGap = dot_vertical_gap;
+    }
+
+
+    // Read boolean preferences
+    Tuple *show_year_t = dict_find(iterator, MESSAGE_KEY_ShowYear);
+    if (show_year_t) {
+        bool show_year = show_year_t->value->int32 == 1;
+        s_settings->ShowYear = show_year;
+    }
+
+    Tuple *show_seconds_t = dict_find(iterator, MESSAGE_KEY_ShowSeconds);
+    if (show_seconds_t) {
+        bool show_seconds = show_seconds_t->value->int32 == 1;
+        s_settings->ShowSeconds = show_seconds;
+    }
+
+    Tuple *show_weekday_t = dict_find(iterator, MESSAGE_KEY_ShowWeekdayAbbreviation);
+    if (show_weekday_t) {
+        bool show_weekday = show_weekday_t->value->int32 == 1;
+        s_settings->ShowWeekdayAbbreviation = show_weekday;
+    }
+
+    Tuple *weekday_uppercase_t = dict_find(iterator, MESSAGE_KEY_WeekdayAbbreviationUppercase);
+    if (weekday_uppercase_t) {
+        bool weekday_uppercase = weekday_uppercase_t->value->int32 == 1;
+        s_settings->WeekdayAbbreviationUppercase = weekday_uppercase;
+    }
+
+    Tuple *row0_t = dict_find(iterator, MESSAGE_KEY_Row0Widget);
+    if (row0_t) {
+        // Read as a string and convert to integer
+        int value = atoi(row0_t->value->cstring);
+        if (s_settings->Row0Widget != value) {
+            s_settings->Row0Widget = value;
+        }
+    }
+
+    Tuple *row1_t = dict_find(iterator, MESSAGE_KEY_Row1Widget);
+    if (row1_t) {
+        int value = atoi(row1_t->value->cstring);
+        if (s_settings->Row1Widget != value) {
+            s_settings->Row1Widget = value;
+        }
+    }
+
+    Tuple *row3_t = dict_find(iterator, MESSAGE_KEY_Row3Widget);
+    if (row3_t) {
+        int value = atoi(row3_t->value->cstring);
+        if (s_settings->Row3Widget != value) {
+            s_settings->Row3Widget = value;
+        }
+    }
+
+    Tuple *row4_t = dict_find(iterator, MESSAGE_KEY_Row4Widget);
+    if (row4_t) {
+        int value = atoi(row4_t->value->cstring);
+        if (s_settings->Row4Widget != value) {
+            s_settings->Row4Widget = value;
+        }
+    }
+
+    // Read weather data
+
+    // Read tuples for data
+    Tuple *temp_cur_tuple = dict_find(iterator, MESSAGE_KEY_WEATHER_TEMPERATURE_CURRENT);
+    Tuple *temp_min_tuple = dict_find(iterator, MESSAGE_KEY_WEATHER_TEMPERATURE_MIN);
+    Tuple *temp_max_tuple = dict_find(iterator, MESSAGE_KEY_WEATHER_TEMPERATURE_MAX);
+    Tuple *condition_tuple = dict_find(iterator, MESSAGE_KEY_WEATHER_CONDITION);
+
+    // If all data is available, use it
+    if (temp_cur_tuple && temp_min_tuple && temp_max_tuple && condition_tuple) {
+        WeatherData *weatherData = weather_get_data();
+
+        APP_LOG(APP_LOG_LEVEL_DEBUG, "temp changed: old: %d new: %d", (int) weatherData->CurrentTemperature, (int)temp_cur_tuple->value->int32);
+
+        weatherData->CurrentTemperature = temp_cur_tuple->value->int32;
+        weatherData->MinTemperature = temp_min_tuple->value->int32;
+        weatherData->MaxTemperature = temp_max_tuple->value->int32;
+        strcpy(weatherData->CurrentConditions, condition_tuple->value->cstring);
+
+        update_weather();
+    }
+
+    // persist data
+    clay_save_settings();
+
     main_reload_layout();
-  }
 }
 
 static void inbox_dropped_callback(AppMessageResult reason, void *context) {
-  APP_LOG(APP_LOG_LEVEL_ERROR, "Message dropped! %d", (int) reason);
+    APP_LOG(APP_LOG_LEVEL_ERROR, "Message dropped! %d", (int) reason);
 }
 
 static void outbox_failed_callback(DictionaryIterator *iterator, AppMessageResult reason, void *context) {
-  APP_LOG(APP_LOG_LEVEL_ERROR, "Outbox send failed! %d", (int) reason);
+    APP_LOG(APP_LOG_LEVEL_ERROR, "Outbox send failed! %d", (int) reason);
 }
 
 static void outbox_sent_callback(DictionaryIterator *iterator, void *context) {
-  APP_LOG(APP_LOG_LEVEL_INFO, "Outbox send success!");
+    APP_LOG(APP_LOG_LEVEL_INFO, "Outbox send success!");
 }
 
 void initialize_app_messaging() {
-  // Register callbacks
-  app_message_register_inbox_received(inbox_received_callback);
-  app_message_register_inbox_dropped(inbox_dropped_callback);
-  app_message_register_outbox_failed(outbox_failed_callback);
-  app_message_register_outbox_sent(outbox_sent_callback);
+    // Register callbacks
+    app_message_register_inbox_received(inbox_received_callback);
+    app_message_register_inbox_dropped(inbox_dropped_callback);
+    app_message_register_outbox_failed(outbox_failed_callback);
+    app_message_register_outbox_sent(outbox_sent_callback);
 
-  // Open AppMessage with the largest receive buffer the platform supports,
-  // to accommodate all settings including string fields like WeatherApiKey.
-  app_message_open(app_message_inbox_size_maximum(), 256);
+    // Open AppMessage with the largest receive buffer the platform supports,
+    // to accommodate all settings including string fields like WeatherApiKey.
+    app_message_open(app_message_inbox_size_maximum(), 256);
 }
