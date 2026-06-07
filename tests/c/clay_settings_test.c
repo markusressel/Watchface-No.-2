fc4d322dd1c45bd9f041ae7383057407c36110ba
Markus Ressel <mail@markusressel.de>
2026-06-08 00:59:39 +0200

    cannot test emery builds in unit tests right now, because it would require a build argument, fix tests by using non-emery defaults for now

--- tests/c/clay_settings_test.c
+++ tests/c/clay_settings_test.c
@@ -49,16 +49,15 @@ void test_clay_load_settings_with_valid_version(void) {
     mock_storage_set_version(SETTINGS_VERSION);
 
     ClaySettings *saved_settings = clay_default_settings();
-    saved_settings->LayoutRowCount = layout_row_count_max_for_platform() - 2;
+    saved_settings->LayoutRowCount = layout_row_count_max_for_platform();
     saved_settings->ShowSeconds = true;
     clay_save_settings(saved_settings);
-    // mock_storage_set_data(&saved_settings, sizeof(ClaySettings));
 
     // Act
     ClaySettings *loaded_settings = clay_load_settings();
 
     // Assert: Check that settings were loaded from storage
-    TEST_ASSERT_EQUAL_INT(layout_row_count_max_for_platform() - 2, loaded_settings->LayoutRowCount);
+    TEST_ASSERT_EQUAL_INT(layout_row_count_max_for_platform(), loaded_settings->LayoutRowCount);
     TEST_ASSERT_TRUE(loaded_settings->ShowSeconds);
 }
 
