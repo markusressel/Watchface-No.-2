72ea07b63ebbe43e5ce69cfa052be15bf4d2fd9a
Markus Ressel <mail@markusressel.de>
2026-06-08 22:40:54 +0200

    Refactor directory structure

--- src/pkjs/weather/weather.js
+++ src/pkjs/weather/weather.js
@@ -2,7 +2,7 @@ let config = require('../config/config');
 let appMessaging = require('../app_messaging');
 let owm = require("./openweathermap");
 let Persistence = require('../persistence');
-let timelineSimulation = require('../timeline.json');
+let timelineSimulation = require('./mock/timeline.json');
 
 
 let WEATHER_UPDATE_INTERVAL_MS = 30.0.minutes;
